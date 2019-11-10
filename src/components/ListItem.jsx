7bcdb80e29d6b96a30fbf5f4c1e7422bf6629be6
Indiana Jones <flyoungiv@gmail.com>
2019-11-09 20:38:28 -0600

    some stuff

--- src/components/ListItem.jsx
+++ src/components/ListItem.jsx
@@ -1,21 +1,32 @@
 import React from 'react'
+import link from '../images/link.svg'
 
 const listItemStyle = {
   width: '1200px',
   minHeight: '60px',
   borderRadius: '4px',
-  backgroundColor: 'burlywood',
+  backgroundColor: 'white',
   margin: '.5rem',
   display: 'flex',
   alignItems: 'center',
 }
 
 const smallColumn = {
-  flex: '0 0 10%'
+  flex: '0 0 10%',
 }
 
 const bigColumn = {
-  flex: '0 0 40%'
+  flex: '0 0 40%',
+  padding: '24px',
+}
+
+const patchStyle = {
+  width: '50%',
+  padding: '10%',
+}
+
+const linkStyle = {
+  width: '20%',
 }
 
 export default function ListItem(props) {
@@ -23,14 +34,18 @@ export default function ListItem(props) {
   const { flightData } = props
 
   return (
-    <div style={listItemStyle}>
-      <span style={smallColumn}>Badge</span>
+    <div style={listItemStyle} className="paper">
+      <span style={smallColumn}>
+        <img alt="badge" src={flightData.links.mission_patch_small} style={patchStyle} />
+      </span>
       <span style={smallColumn}>{flightData.mission_name}</span>
       <span style={smallColumn}>{flightData.rocket.rocket_type}</span>
       <span style={smallColumn}>{flightData.launch_date_utc}</span>
       <span style={bigColumn}>{flightData.details}</span>
       <span style={smallColumn}>{flightData.flight_numbner}</span>
-      <span style={smallColumn}><a href={flightData.links.article_link}>Article</a></span>
+      <span style={smallColumn}>
+        <a href={flightData.links.article_link}><img alt="link" style={linkStyle} src={link} /></a>
+      </span>
     </div>
   )
 }
